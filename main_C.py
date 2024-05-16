ad797c089131fe7d346df1e2220050c137aceb87
Ramez <Ramez.gammoh.2004@gmail.com>
2024-05-16 15:09:41 +0200

    finished with all 3 files

--- main_C.py
+++ main_C.py
@@ -1,28 +1,34 @@
 import glob
 import os
-
 import mne
 from matplotlib import pyplot as plt
 
 
-def Calculate_oxy_data(snirf_file):
-    raw = mne.io.read_raw_snirf(snirf_file, preload=True)
-    raw = mne.preprocessing.nirs.optical_density(raw)
-    hbo_data = mne.preprocessing.nirs.beer_lambert_law(raw)
-    return hbo_data
+def Calculate_oxy_data(raw_ob):
+    """Function to calculate oxy data"""
+    hbo_data = mne.preprocessing.nirs.beer_lambert_law(raw_ob)
+    return hbo_data.get_data()
 
 
 def main():
-    directory_path = 'snrif_files'
-    files = glob.glob(os.path.join(directory_path, "*.snirf"))
-    oxy_data_accumulative_list = []
+    directory_path = 'replace with snirf files directory'  # gets the snrif files for processing
+    files = glob.glob(os.path.join(directory_path, "*.snirf"))  # gets only snirf files from the folder
+    # making variables
+    oxy_data_transpose = []
+    raw_times = []
+    raw = None
+    # loop that goes through every file
     for file_path in files:
-        oxy_data = Calculate_oxy_data(file_path)
-        oxy_data_accumulative_list.extend(oxy_data.get_data())  # Accumulate oxy data from all files
-    plt.hist(oxy_data_accumulative_list, bins=50, color='blue', edgecolor='black')  # Plot histogram for all data
-    plt.xlabel('Oxyhemoglobin Values')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Oxyhemoglobin Values')
+        # gathering data in lists to plot
+        raw = mne.io.read_raw_snirf(file_path, preload=True)
+        raw = mne.preprocessing.nirs.optical_density(raw)
+        oxy_data_transpose.extend(Calculate_oxy_data(raw).T)
+        raw_times.extend(raw.times)
+    # plotting the data
+    plt.plot(raw_times, oxy_data_transpose)
+    plt.xlabel('Time (s)')
+    plt.ylabel('HbO Concentration (AU)')
+    plt.title('Oxyhemoglobin (HbO) Concentration')
     plt.show()
 
 
